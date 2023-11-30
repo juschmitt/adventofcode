813563f1337f35dfc71f0f926fd6d3751b0ba9ee
Julian Schmitt <julian.schmitt@ioki.com>
2023-11-30 17:01:38 +0100

    prepare for day1

---
+++ src/main/kotlin/utils/Day.kt
@@ -0,0 +1,44 @@
+package utils
+
+import java.io.BufferedReader
+import java.io.File
+
+abstract class Day(
+    private val dayNumber: Int,
+    private val year: Int,
+    private val useSampleInput: Boolean = true
+) {
+    val inputList: List<String> by lazy {
+        InputReader.getInputAsList(dayNumber, year, useSampleInput)
+    }
+    val inputString: String by lazy {
+        InputReader.getInputAsString(dayNumber, year, useSampleInput)
+    }
+
+    abstract fun partOne(): Any
+    abstract fun partTwo(): Any
+    fun printDay() {
+        val header = "Day $dayNumber, $year"
+        val footer = "—".repeat(header.length)
+
+        println(header)
+        println("Part 1: ${partOne()}")
+        println("Part 2: ${partTwo()}")
+        println(footer)
+    }
+}
+
+object InputReader {
+
+    fun getInputAsString(day: Int, year: Int, useSampleInput: Boolean): String =
+        fileReader(day, year, useSampleInput).readText()
+
+    fun getInputAsList(day: Int, year: Int, useSampleInput: Boolean): List<String> =
+        fileReader(day, year, useSampleInput).readLines()
+
+    private fun fileReader(day: Int, year: Int, useSampleInput: Boolean): BufferedReader =
+        (if (useSampleInput) "day${day}_input_test" else "day${day}_input").let {
+            File("src/main/kotlin/y$year/inputs/$it.txt")
+                .bufferedReader()
+        }
+}
---
+++ src/main/kotlin/y2023/Day1.kt
@@ -0,0 +1,13 @@
+package y2023
+
+import utils.Day
+
+class Day1 : Day(1, 2023) {
+    override fun partOne(): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun partTwo(): Any {
+        TODO("Not yet implemented")
+    }
+}
