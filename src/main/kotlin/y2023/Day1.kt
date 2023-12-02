837ecae8b66061481d0492bd2664a478aef185ce
Julian Schmitt <juschmitt91@gmail.com>
2023-12-02 18:06:35 +0100

    day 1 part 2

--- src/main/kotlin/y2023/Day1.kt
+++ src/main/kotlin/y2023/Day1.kt
@@ -4,14 +4,32 @@ import utils.Day
 
 class Day1(useSampleInput: Boolean) : Day(1, 2023, useSampleInput) {
     override fun partOne(): Any {
-        return inputList.findSumOfFirstAndLast { it.isDigit() }
+        return inputList.findSumOfFirstAndLastDigit()
     }
 
     override fun partTwo(): Any {
-        return Unit
+        return inputList.mapNumberNameToNumeric().findSumOfFirstAndLastDigit()
     }
 }
 
-private fun List<String>.findSumOfFirstAndLast(comp: (Char) -> Boolean): Int = map { line ->
-    "${line.first { comp(it) }}${line.last { comp(it) }}"
+private fun List<String>.findSumOfFirstAndLastDigit(): Int = map { line ->
+    "${line.first { it.isDigit() }}${line.last { it.isDigit() }}"
 }.sumOf { it.toInt() }
+
+private fun List<String>.mapNumberNameToNumeric(): List<String> = map { line ->
+    var newline = line
+    Number.entries.forEach { num -> newline = newline.replace(num.name, num.numeric, true) }
+    newline
+}
+
+private enum class Number(val numeric: String) {
+    ONE("o1e"),
+    TWO("t2o"),
+    THREE("t3e"),
+    FOUR("f4r"),
+    FIVE("f5e"),
+    SIX("s6x"),
+    SEVEN("s7n"),
+    EIGHT("e8t"),
+    NINE("n9e"),
+}
