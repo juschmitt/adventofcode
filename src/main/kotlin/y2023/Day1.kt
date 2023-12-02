bf4421acc65e07bfe546651b2cd8a56b68c38de8
Julian Schmitt <juschmitt91@gmail.com>
2023-12-03 00:32:20 +0100

    day 1 part 2 rework

--- src/main/kotlin/y2023/Day1.kt
+++ src/main/kotlin/y2023/Day1.kt
@@ -4,32 +4,52 @@ import utils.Day
 
 class Day1(useSampleInput: Boolean = false) : Day(1, 2023, useSampleInput) {
     override fun partOne(): Any {
-        return inputList.findSumOfFirstAndLastDigit()
+        return inputList.findSumOfFirstAndLastDigit().sum()
     }
 
     override fun partTwo(): Any {
-        return inputList.mapNumberNameToNumeric().findSumOfFirstAndLastDigit()
+        return inputList.findSumOfFirstAndLastNumber().sum()
     }
 }
 
-private fun List<String>.findSumOfFirstAndLastDigit(): Int = map { line ->
-    "${line.first { it.isDigit() }}${line.last { it.isDigit() }}"
-}.sumOf { it.toInt() }
-
-private fun List<String>.mapNumberNameToNumeric(): List<String> = map { line ->
-    var newline = line
-    Number.entries.forEach { num -> newline = newline.replace(num.name, num.numeric, true) }
-    newline
+private fun List<String>.findSumOfFirstAndLastDigit(): List<Int> = map { line ->
+    line.first { it.isDigit() }.digitToInt() * 10 + line.last { it.isDigit() }.digitToInt()
 }
 
-private enum class Number(val numeric: String) {
-    ONE("o1e"),
-    TWO("t2o"),
-    THREE("t3e"),
-    FOUR("f4r"),
-    FIVE("f5e"),
-    SIX("s6x"),
-    SEVEN("s7n"),
-    EIGHT("e8t"),
-    NINE("n9e"),
+private fun List<String>.findSumOfFirstAndLastNumber(): List<Int> = map { line ->
+    line.findFirstNumber() * 10 + line.findLastNumber()
+}
+
+private fun String.findFirstNumber(): Int {
+    var startIdx = 0
+    (1 .. length).forEach { i ->
+        val current = substring(startIdx, i)
+        Number.entries.forEach { if(current.contains(it.name.lowercase())) { return it.ordinal+1 } }
+        current.firstOrNull { it.isDigit() }?.let { return it.digitToInt() }
+        if (i > 5) startIdx++
+    }
+    error("Something wrong here. No numbers in $this")
+}
+
+private fun String.findLastNumber(): Int {
+    var endIdx = length
+    (length - 1 downTo 0).forEach { i ->
+        val current = substring(i, endIdx)
+        Number.entries.forEach { if(current.contains(it.name.lowercase())) { return it.ordinal+1 } }
+        current.lastOrNull { it.isDigit() }?.let { return it.digitToInt() }
+        if (i < length-5) endIdx--
+    }
+    error("Something wrong here. No numbers in $this")
+}
+
+private enum class Number {
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
 }
