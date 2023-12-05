d8100f1eb45302366153c4fd5b4222cc213c75d3
Julian Schmitt <juschmitt91@gmail.com>
2023-12-06 00:15:36 +0100

    day 5 cleanup

--- src/main/kotlin/y2023/Day5.kt
+++ src/main/kotlin/y2023/Day5.kt
@@ -4,30 +4,29 @@ import utils.Day
 
 class Day5(useSampleInput: Boolean = false) : Day(5, 2023, useSampleInput) {
     override fun partOne(): Any {
-        return inputString.parseAlmanac().findNearestLocation()
+        return inputString.parseAlmanac().run {
+            seeds.minOf { seed ->
+                maps.findLocationForSeed(seed)
+            }
+        }
     }
-
 
     override fun partTwo(): Any {
-        return inputString.parseAlmanac().findNearestLocation2()
-    }
-}
-
-private fun Almanac.findNearestLocation() = seeds.minOf { seed ->
-    maps.fold(seed) { cur, map ->
-        val mapping = map.find { (source, _) -> source.contains(cur) }
-        mapping?.let { (source, dest) -> dest + (cur - source.first) } ?: cur
-    }
-}
-
-private fun Almanac.findNearestLocation2() = seeds.chunked(2).minOf { (start, range) ->
-    (start..<(start+range)).minOf { seed ->
-        maps.fold(seed) { cur, map ->
-            val mapping = map.find { (source, _) -> source.contains(cur) }
-            mapping?.let { (source, dest) -> dest + (cur - source.first) } ?: cur
+        return inputString.parseAlmanac().run {
+            seeds.chunked(2).minOf { (start, range) ->
+                (start..<(start + range)).minOf { seed ->
+                    maps.findLocationForSeed(seed)
+                }
+            }
         }
     }
 }
+
+private fun List<List<Pair<LongRange, LongRange>>>.findLocationForSeed(seed: Long) =
+    fold(seed) { cur, map ->
+        val mapping = map.find { (source, _) -> source.contains(cur) }
+        mapping?.let { (source, dest) -> dest.first + (cur - source.first) } ?: cur
+    }
 
 private fun String.parseAlmanac(): Almanac {
     val (head, tail) = split("\n\n").run { Pair(first(), drop(1)) }
@@ -39,11 +38,11 @@ private fun String.parseAlmanac(): Almanac {
 private fun List<String>.parseMappings() = map { map ->
     map.split("\n").drop(1).map { line ->
         val (dest, source, range) = line.split(" ").map { it.toLong() }
-        source..<(source + range) to dest
+        source..<(source + range) to dest..<(dest + range)
     }
 }
 
 private data class Almanac(
     val seeds: List<Long>,
-    val maps: List<List<Pair<LongRange, Long>>>
+    val maps: List<List<Pair<LongRange, LongRange>>>
 )
