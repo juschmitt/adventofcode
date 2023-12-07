be458c3f89cf8c6d1033f7557b1484ab254ce95c
Julian Schmitt <juschmitt91@gmail.com>
2023-12-07 14:15:51 +0100

    day 8 setup

---
+++ src/main/kotlin/y2023/Day8.kt
@@ -0,0 +1,13 @@
+package y2023
+
+import utils.Day
+
+class Day8 : Day(8, 2023, true) {
+    override fun partOne(): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun partTwo(): Any {
+        TODO("Not yet implemented")
+    }
+}
