3220c142adc431f6d65111b9623be188237b6669
Julian Schmitt <juschmitt91@gmail.com>
2023-12-10 01:06:05 +0100

    day 9 prep

---
+++ src/main/kotlin/y2023/Day9.kt
@@ -0,0 +1,13 @@
+package y2023
+
+import utils.Day
+
+class Day9 : Day(9, 2023, true) {
+    override fun partOne(): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun partTwo(): Any {
+        TODO("Not yet implemented")
+    }
+}
--- src/main/kotlin/y2023/Main.kt
+++ src/main/kotlin/y2023/Main.kt
@@ -1,12 +1,13 @@
 package y2023
 
 fun main() {
-    //Day1().printDay()
-    //Day2().printDay()
-    //Day3().printDay()
-    //Day4().printDay()
-    //Day5().printDay()
-    //Day6().printDay()
+//    Day1().printDay()
+//    Day2().printDay()
+//    Day3().printDay()
+//    Day4().printDay()
+//    Day5().printDay()
+//    Day6().printDay()
 //    Day7().printDay()
-    Day8().printDay()
+//    Day8().printDay()
+    Day9().printDay()
 }
