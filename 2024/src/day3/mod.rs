0738a7db7d70352d695da7d5cb34436db00454df
Julian Schmitt <juschmitt91@gmail.com>
2024-12-02 16:18:42 +0100

    prepare day 3

---
+++ 2024/src/day3/mod.rs
@@ -0,0 +1,30 @@
+pub fn day3() {
+    todo!()
+}
+
+fn day3_part1(input: &str) -> i32 {
+    todo!()
+}
+
+fn day3_part2(input: &str) -> i32 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = include_str!("test_input.txt");
+
+    #[test]
+    fn test_day3_part1() {
+        assert_eq!(day3_part1(TEST_INPUT), 0);
+    }
+
+    #[test]
+    fn test_day3_part2() {
+        assert_eq!(day3_part2(TEST_INPUT), 0);
+    }
+}
+
+const INPUT: &str = include_str!("input.txt");
