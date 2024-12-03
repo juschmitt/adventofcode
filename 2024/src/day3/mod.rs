74b49c2be79d14bfac476db6ca0b11fbbf8b2f54
Julian Schmitt <juschmitt91@gmail.com>
2024-12-03 15:15:24 +0100

    add day3part2

--- 2024/src/day3/mod.rs
+++ 2024/src/day3/mod.rs
@@ -1,3 +1,5 @@
+use crate::day3::Instruction::Mul;
+
 pub fn day3() {
     let part1_result = day3_part1(INPUT);
     println!("Day 3, Part 1: {}", part1_result);
@@ -6,40 +8,111 @@ pub fn day3() {
 }
 
 fn day3_part1(input: &str) -> i32 {
-    parse_input(input)
-        .iter()
-        .map(|(a, b)| a * b)
-        .sum()
+    parse_input(input).iter().map(|(a, b)| a * b).sum()
 }
 
 fn day3_part2(input: &str) -> i32 {
-    0
+    parse_input_conditional(input)
+        .iter()
+        .fold((0, true), |acc, x| match x {
+            Mul(a, b) => {
+                if acc.1 {
+                    (acc.0 + a * b, acc.1)
+                } else {
+                    acc
+                }
+            }
+            Instruction::Do => (acc.0, true),
+            Instruction::Dont => (acc.0, false),
+        })
+        .0
 }
 
 fn parse_input(input: &str) -> Vec<(i32, i32)> {
     let r = regex::Regex::new(r"mul\(\d+,\d+\)").unwrap();
-    r.find_iter(input).map(|m| m.as_str().split(","))
-    .map(|mut x| {
-        let a = x.next().unwrap().strip_prefix("mul(").unwrap().parse().unwrap();
-        let b = x.next().unwrap().strip_suffix(")").unwrap().parse().unwrap();
-        (a, b)
-    }).collect()
+    r.find_iter(input)
+        .map(|m| m.as_str().split(","))
+        .map(|mut x| {
+            let a = x
+                .next()
+                .unwrap()
+                .strip_prefix("mul(")
+                .unwrap()
+                .parse()
+                .unwrap();
+            let b = x
+                .next()
+                .unwrap()
+                .strip_suffix(")")
+                .unwrap()
+                .parse()
+                .unwrap();
+            (a, b)
+        })
+        .collect()
+}
+
+fn parse_input_conditional(input: &str) -> Vec<Instruction> {
+    let r = regex::Regex::new(
+        r#"(?x)
+            (mul\(\d+,\d+\)) |
+            (do\(\)) |
+            (don't\(\))
+            "#,
+    )
+    .unwrap();
+
+    r.find_iter(input)
+        .map(|m| m.as_str())
+        .map(|x| {
+            if x.starts_with("mul") {
+                let mut x = x.split(",");
+                let a = x
+                    .next()
+                    .unwrap()
+                    .strip_prefix("mul(")
+                    .unwrap()
+                    .parse()
+                    .unwrap();
+                let b = x
+                    .next()
+                    .unwrap()
+                    .strip_suffix(")")
+                    .unwrap()
+                    .parse()
+                    .unwrap();
+                Mul(a, b)
+            } else {
+                match x {
+                    "don't()" => Instruction::Dont,
+                    "do()" => Instruction::Do,
+                    &_ => panic!("Regex parsing failed."),
+                }
+            }
+        })
+        .collect()
+}
+
+enum Instruction {
+    Mul(i32, i32),
+    Do,
+    Dont,
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const TEST_INPUT: &str = include_str!("test_input.txt");
-
     #[test]
     fn test_day3_part1() {
-        assert_eq!(day3_part1(TEST_INPUT), 161);
+        let test_input: &str = include_str!("test_input.txt");
+        assert_eq!(day3_part1(test_input), 161);
     }
 
     #[test]
     fn test_day3_part2() {
-        assert_eq!(day3_part2(TEST_INPUT), 0);
+        let test_input: &str = include_str!("test_input_part2.txt");
+        assert_eq!(day3_part2(test_input), 48);
     }
 }
 
